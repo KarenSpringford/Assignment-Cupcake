3e1e0a3632af4b0761d11d01e1317baccf9f4704
KarenSpringford <jkspringford@gmail.com>
2016-07-29 11:12:36 -0400

    Add project files.

---
+++ Assignment2-Cupcake/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2_Cupcake.Startup))]
+namespace Assignment2_Cupcake
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
